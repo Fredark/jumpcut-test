3fe2357030d573c2e48b6b93b4684a7cd8b397a1
Fred <fred.webfront@gmail.com>
2019-01-06 21:16:53 -0200

    Finish desktop

---
+++ build/precache-manifest.3569fae58e724008a280a4d7b6c6c97b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "002615435c5c74ecd920d40bcc102ec4",
+    "url": "/static/media/banner.00261543.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fb1c5fc2e1c0e5467ec",
+    "url": "/static/js/main.2fb1c5fc.chunk.js"
+  },
+  {
+    "revision": "abc8b0d7630f34f32649",
+    "url": "/static/js/1.abc8b0d7.chunk.js"
+  },
+  {
+    "revision": "2fb1c5fc2e1c0e5467ec",
+    "url": "/static/css/main.c25d3a8a.chunk.css"
+  },
+  {
+    "revision": "ca78386630a24c47b3a22368338d9d59",
+    "url": "/index.html"
+  }
+];
