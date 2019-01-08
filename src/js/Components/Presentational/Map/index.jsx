f6976c4a7ae95c0b4332d2b66725b99340567866
Fred <fred.webfront@gmail.com>
2019-01-08 04:32:39 -0200

    Fixed

--- src/js/Components/Presentational/Map/index.jsx
+++ src/js/Components/Presentational/Map/index.jsx
@@ -27,7 +27,7 @@ class Map extends Component {
             <button className="map__button" type="button">Advanced Search</button>
           </div>
           <div className="map__map" id="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423283.44333751354!2d-118.69261204759972!3d34.02072893630757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos+Angeles%2C+CA%2C+EUA!5e0!3m2!1spt-BR!2sbr!4v1546927044292" width="600" height="450" frameBorder="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423283.44333751354!2d-118.69261204759972!3d34.02072893630757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos+Angeles%2C+CA%2C+EUA!5e0!3m2!1spt-BR!2sbr!4v1546927044292" width="600" height="450" frameBorder="0" title="map"></iframe>
           </div>
         </div>
       </section>
