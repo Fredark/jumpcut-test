4b9393355396c18938e3a8e333b5990ee2a99bb9
Fred <fred.webfront@gmail.com>
2019-01-08 06:15:32 -0200

    part two

---
+++ src/js/Components/Presentational/Mask/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Icons from '../Icons';
+import './mask.scss';
+
+class Mask extends Component {
+
+  render() {
+    return (
+      <div className="mask">
+        <Icons id="logo" />
+      </div>
+    );
+  }
+}
+
+export default Mask;
--- src/js/Pages/Home/index.js
+++ src/js/Pages/Home/index.js
@@ -5,10 +5,47 @@ import Search from '../../Components/Container/Search';
 import Map from '../../Components/Presentational/Map';
 import UsersList from '../../Components/Container/UsersList';
 import Footer from '../../Components/Presentational/Footer';
+import Mask from '../../Components/Presentational/Mask';
 import './Home.scss';
 
 
 class Home extends Component {
+
+  componentDidMount() {
+
+    window.addEventListener('load', (event) => {
+      document.body.classList.add('page--loaded');
+      document.querySelector('.header').classList.add('section--active');
+      document.querySelector('.search').classList.add('section--active');
+    });
+
+    window.addEventListener('scroll', this.handleScroll);
+
+  }
+
+  handleScroll(event) {
+    let $home        = document.querySelector('.home'),
+        $header      = document.querySelector('.header'),
+        windowHeight = window.outerHeight,
+        scrollY      = window.scrollY;
+
+    [...$home.children].forEach((item) => {
+      if(!item.classList.contains('section--active')) {
+
+        if(scrollY + 150 >= item.offsetTop - (windowHeight / 2)) {
+          item.classList.add('section--active');
+        }
+      }
+
+    });
+
+    if(scrollY >= 100) {
+      $header.classList.add('header--active');
+    } else {
+      $header.classList.remove('header--active');
+    }
+  }
+
   render() {
     return (
       <div className="home">
@@ -17,6 +54,7 @@ class Home extends Component {
         <Map />
         <UsersList />
         <Footer />
+        <Mask />
       </div>
     );
   }
